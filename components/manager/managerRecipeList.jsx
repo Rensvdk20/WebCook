73644472d3727e8baa6d2306e1282103e1087ff5
Rensvdk20 <rensvdk@gmail.com>
2022-09-02 23:21:04 +0200

    Added managerRecipeList styling and image

--- components/manager/managerRecipeList.jsx
+++ components/manager/managerRecipeList.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function ManagerRecipeList({ allRecipes }) {
 	const recipes = allRecipes.results;
 
@@ -5,7 +7,21 @@ export default function ManagerRecipeList({ allRecipes }) {
 		if (!allRecipes.error) {
 			return recipes.map((recipe) => (
 				<div className="col-lg-12 col-sm-12 col-xs-12" key={recipe.id}>
-					{recipe.name}
+					<div className="recipe">
+						<div className="recipe-image">
+							<Image
+								src={
+									process.env.NEXT_PUBLIC_IMAGE_URL +
+									recipe.image
+								}
+								alt={recipe.title}
+								layout="responsive"
+								width={500}
+								height={300}
+								objectFit="cover"
+							/>
+						</div>
+					</div>
 				</div>
 			));
 		} else {
@@ -15,7 +31,7 @@ export default function ManagerRecipeList({ allRecipes }) {
 
 	return (
 		<div className="manager-recipe-list">
-			<div className="row">{/* showRecipes() */}</div>
+			<div className="row">{showRecipes()}</div>
 		</div>
 	);
 }
